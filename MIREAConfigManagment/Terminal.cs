2ab29ae4eed4af45492e3060a905c475271a5a40
isNIKOLAY <nikolayurch@gmail.com>
2026-09-24 17:12:30 +0300

    feat: add main structure of the CLI

---
+++ MIREAConfigManagment/Terminal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MIREAConfigManagment
+{
+    internal class Terminal
+    {
+        public Terminal()
+        {
+            Global.Username = Environment.UserName;
+            Global.Hostname = Environment.MachineName;
+        }
+
+        internal void Start()
+        {
+            while (true)
+            {
+                Console.WriteLine($"{Global.Username}@{Global.Hostname}:~$");
+
+                string? input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                    continue;
+            }    
+        }
+    }
+}

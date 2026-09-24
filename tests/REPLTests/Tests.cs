76dc9e94748f29a1d829df330ad326689d515e78
isNIKOLAY <nikolayurch@gmail.com>
2026-09-25 00:50:33 +0300

    tests: add tests for commands ls, cd, random unkown command and resolve env vars

---
+++ tests/REPLTests/Tests.cs
@@ -0,0 +1,93 @@
+﻿using MIREAConfigManagment;
+using MIREAConfigManagment.Commands;
+
+namespace REPLTests
+{
+    public class Tests
+    {
+        // Обработка ввода рандомного текста на вход в командный движок
+        [Fact]
+        public void RandomTextCommandType()
+        {
+            var standartOut = Console.Out;
+            using var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            try
+            {
+                var rnd = new Random();
+                string input = rnd.GetHexString(rnd.Next(50));
+
+                CommandsEngine.Parse(input);
+
+                Assert.Equal($"Command not found", stringWriter.ToString().Replace(Environment.NewLine, ""));
+            }
+            finally
+            {
+                Console.SetOut(standartOut);
+            }
+        }
+
+        // Проверка команды cd
+        [Theory]
+        [InlineData("cd", "cd")]
+        [InlineData("cd --test-param", "cd --test-param")]
+        [InlineData("cd --test-param1 --test-param2", "cd --test-param1 --test-param2")]
+        public void CheckCdCommandResult(string input, string output)
+        {
+            var standartOut = Console.Out;
+            using var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            try
+            {
+                CommandsEngine.Parse(input);
+
+                Assert.Equal(output, stringWriter.ToString().Replace(Environment.NewLine, ""));
+            }
+            finally
+            {
+                Console.SetOut(standartOut);
+            }
+        }
+
+        // Проверка команды ls
+        [Theory]
+        [InlineData("ls", "ls")]
+        [InlineData("ls --test-param", "ls --test-param")]
+        [InlineData("ls --test-param1 --test-param2", "ls --test-param1 --test-param2")]
+        public void CheckLsCommandResult(string input, string output)
+        {
+            var standartOut = Console.Out;
+            using var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            try
+            {
+                CommandsEngine.Parse(input);
+
+                Assert.Equal(output, stringWriter.ToString().Replace(Environment.NewLine, ""));
+            }
+            finally
+            {
+                Console.SetOut(standartOut);
+            }
+        }
+
+        // Проверка обработки переменных окружения
+        [Theory]
+        [InlineData("echo $TEST1", "echo 12345")]
+        [InlineData("echo params $TEST1", "echo params 12345")]
+        [InlineData("echo $TEST2 params test", "echo 54321 params test")]
+        [InlineData("echo params $TEST2 check test", "echo params 54321 check test")]
+        public void TestEnvVars(string input, string output)
+        {
+            Environment.SetEnvironmentVariable("TEST1", "12345");
+            Environment.SetEnvironmentVariable("TEST2", "54321");
+
+            string result = Terminal.ResolveEnvVariables(input);
+
+            Assert.Equal(output, result);
+        }
+    }
+}
